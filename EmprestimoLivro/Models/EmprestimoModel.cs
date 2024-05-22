30a4fa9c5dcf25367bb853929c9edeb17b392745
Gabriel Gouveia <gabrielgouveia371@gmail.com>
2024-05-21 23:29:56 -0300

    Finalização do projeto MVC

---
+++ EmprestimoLivro/Models/EmprestimoModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmprestimoLivro.Models
+{
+    public class EmprestimoModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Digita o nome do recebedor!")]
+        public string Recebedor { get; set; }
+        [Required(ErrorMessage = "Digita o nome do fornecedor!")]
+        public string Fornecedor { get; set; }
+        public DateTime dataUltimaAtualizacao { get; set; } = DateTime.Now;
+        [Required(ErrorMessage = "Digita o nome do livro!")]
+        public string LivroEmprestado { get; set; }
+    }
+}
